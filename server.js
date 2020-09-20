b516d8479f5e28a2d97d806183ecdb5a5d335446
nguyenla <phulamnjc@gmail.com>
2020-09-20 14:37:00 -0400

    Reorganize html files, npm project setup

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const express = require('express'), PORT = 3000;
+const express = require("express");
+
 const app = express();
-const users = [];
 
-app.get('/users', (req, res) => {
-     res.json(users)
-})
+app.get("/", (req, res) => res.send("Hello world!"));
 
-app.listen(3000);
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
