9a75f4195faf01ae2e93dc67ac22344f1fc59fdf
nguyenla <phulamnjc@gmail.com>
2020-09-23 22:41:08 -0400

    Add user model and register route

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Database config
 const db = require("./config/keys").mongoURI;
@@ -13,6 +21,10 @@ mongoose
     .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello world!"));
+
+// Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
