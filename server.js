65390f60b62643161d450b7a2e74c1b921c41366
Lam Nguyen <phulamnjc@gmail.com>
2020-09-24 08:56:17 -0400

    Nodejs and MongoDB setup (#3)
    * Reorganize html files, npm project setup
    
    * Set up MongoDB database connection

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-const express = require('express'), PORT = 3000;
+const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
-const users = [];
 
-app.get('/users', (req, res) => {
-     res.json(users)
-})
+// Database config
+const db = require("./config/keys").mongoURI;
 
-app.listen(3000);
+// Connect to MongoDB database
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello world!"));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
