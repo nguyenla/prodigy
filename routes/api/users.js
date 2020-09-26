e4025fff0593f87d6ac226a4f28504fc511f99b4
nguyenla <phulamnjc@gmail.com>
2020-09-26 12:41:11 -0400

    Handle error when database request fails

--- routes/api/users.js
+++ routes/api/users.js
@@ -14,6 +14,8 @@ const router = express.Router();
 // @access Public
 router.get("/test", (req, res) => res.json("User works!"));
 
+router.get("/login", (req, res) => res.json("Login page front end goes here!"));
+
 // @route GET api/users/register
 // @desc Register new user
 // @access Public
@@ -23,36 +25,40 @@ router.post("/register", (req, res) => {
         return res.status(400).json(errors);
     }
 
-    User.findOne({ email: req.body.email }).then((user) => {
-        if (user) {
-            return res.status(400).json({ email: "Email already exists" });
-        } else {
-            const avatar = gravatar.url(req.body.email, {
-                s: "200", // size
-                r: "pg", // rating
-                d: "mm", // default
-            });
-
-            const new_user = new User({
-                name: req.body.name,
-                username: req.body.username,
-                email: req.body.email,
-                password: req.body.password,
-                avatar,
-            });
-
-            bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(new_user.password, salt, (err, hash) => {
-                    if (err) throw err;
-                    new_user.password = hash;
-                    new_user
-                        .save()
-                        .then((user) => res.json(user))
-                        .catch((err) => console.log(err));
+    User.findOne({ email: req.body.email })
+        .then((user) => {
+            if (user) {
+                return res.status(400).json({ email: "Email already exists" });
+            } else {
+                const avatar = gravatar.url(req.body.email, {
+                    s: "200", // size
+                    r: "pg", // rating
+                    d: "mm", // default
                 });
-            });
-        }
-    });
+
+                const new_user = new User({
+                    name: req.body.name,
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                    avatar,
+                });
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(new_user.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        new_user.password = hash;
+                        new_user
+                            .save()
+                            .then((user) => res.json(user))
+                            .catch((err) => console.log(err));
+                    });
+                });
+            }
+        })
+        .catch((err) => {
+            return res.status(400).json(err);
+        });
 });
 
 // @route GET api/users/login
@@ -68,22 +74,26 @@ router.post("/login", (req, res) => {
     const password = req.body.password;
 
     // Find user by email
-    User.findOne({ email }).then((user) => {
-        if (!user) {
-            errors.email = "User not found.";
-            return res.status(404).json(errors);
-        }
-
-        // Check password
-        bcrypt.compare(password, user.password).then((isMatch) => {
-            if (isMatch) {
-                return res.json({ msg: "Success" });
-            } else {
-                errors.password = "Password incorrect";
-                return res.status(400).json(errors);
+    User.findOne({ email })
+        .then((user) => {
+            if (!user) {
+                errors.email = "User not found.";
+                return res.status(404).json(errors);
             }
+
+            // Check password
+            bcrypt.compare(password, user.password).then((isMatch) => {
+                if (isMatch) {
+                    return res.json({ msg: "Success" });
+                } else {
+                    errors.password = "Password incorrect";
+                    return res.status(400).json(errors);
+                }
+            });
+        })
+        .catch((err) => {
+            return res.status(400).json(err);
         });
-    });
 });
 
 module.exports = router;
